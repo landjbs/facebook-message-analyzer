15e06dd74ea9a7748e4be036b33cc4219a099e6f
landjbs <landonsmith@college.harvard.edu>
2019-03-01 13:40:51 -0500

    pull from are-we-friends

--- facebook-message-analyzer.py
+++ facebook-message-analyzer.py
@@ -2,13 +2,17 @@
 import os
 import json
 import numpy as np
-import pylab as pl
-import datetime
 import pandas as pd
+from collections import Counter
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.utils import to_categorical
 
 CURRENT_DIRECTORY = os.getcwd()
-NUMBER_TO_ANALYZE = 5000
+NUMBER_TO_ANALYZE = 50000
 MESSAGE_THRESHOLD = 100
+FRIEND_CUTOFF = 1000 # number of messages with a person to consider a friend
+WORD_USE_CUTOFF = 10
 
 # FIRST DEFINITIONS
 def get_json_data(chat):
@@ -21,7 +25,7 @@ def get_json_data(chat):
         pass # some things the directory aren't messages (DS_Store, stickers_used, etc.)
 
 def clean_word(word):
-    return (word.lower()).replace("?","")
+    return (word.lower())
 
 # ANALYZE CHATS
 chats = os.listdir(CURRENT_DIRECTORY + "/messages/")[:NUMBER_TO_ANALYZE]
@@ -31,12 +35,9 @@ final_data_times = {}
 final_data_words = {}
 invalid_message_count = 0
 
-print('Analyzing ' + str(min(NUMBER_TO_ANALYZE, len(chats))) + ' chats...')
-
 for chat in chats:
     url = chat + '/message.json'
     json_data = get_json_data(chat)
-    print(chat)
     if json_data != None:
         messages = json_data["messages"]
         if len(messages) >= MESSAGE_THRESHOLD:
@@ -44,88 +45,69 @@ for chat in chats:
 
 sorted_chats.sort(reverse=True)
 
-print('Finished processing chats...')
-
+words_used = []
+message_words = []
+number_messages = []
 
 for i, (messages, chat, messages) in enumerate(sorted_chats):
-    number_messages = {}
-    person_to_times = {}
-    number_words = {}
-    words_used = []
-
-    print(str(i) + " - " + str(len(messages)) + " messages - " + str(chat))
-
     for message in messages:
         try:
-            name = message["sender_name"]
-            time = message["timestamp_ms"]
-            message_content = message["content"]
-
-            number_messages[name] = number_messages.get(name, 0)
-            number_messages[name] += 1
-
-            person_to_times[name] = person_to_times.get(name, [])
-            person_to_times[name].append(datetime.datetime.fromtimestamp(time/1000.0))
-
-            number_words[name] = number_words.get(name, [])
-            messageWords = [clean_word(word) for word in message_content.split()]
-            number_words[name].append(len(messageWords))
-            words_used += messageWords
+            # is person a friend?
+            friend_binary = 1 if len(messages) > FRIEND_CUTOFF else 0
+            number_messages.append(friend_binary)
+            # strings used in message
+            words_list = [clean_word(word) for word in message["content"].split()]
+            words_used += words_list
+            message_words.append(words_list)
         except KeyError:
             # happens for special cases like users who deactivated, unfriended, blocked
             invalid_message_count += 1
+# make set of words used more than 10 and less than 400 times
+times_used = Counter(words_used)
+significant_words = set([k for k,v in times_used.items() if v > 10 and v < 400])
 
-    final_data_messages[i] = number_messages
-    final_data_times[i] = person_to_times
-    final_data_words[i] = number_words
+# matrix to hold word vector for each message
+usage_matrix = np.zeros((len(message_words), len(significant_words)))
 
-print('Found ' + str(invalid_message_count) + ' invalid messages...')
-print('Found ' + str(len(sorted_chats)) + ' chats with ' + str(MESSAGE_THRESHOLD) + ' messages or more')
+friend_vector = []
+# create binary usage vector for words in each message
+for rowCounter, message in enumerate(message_words):
+    friend_vector.append(number_messages[rowCounter])
+    for word in message:
+        for colCounter, word_check in enumerate(significant_words):
+            if word == word_check:
+                usage_matrix[rowCounter, colCounter] = 1
 
+# NEURAL NET TRAINING AND SET UP
+df = pd.DataFrame(data=usage_matrix)
 
+model = Sequential([
+    Dense(300, input_shape=(len(significant_words),)),
+    Activation('relu'),
+    Dense(2),
+    Activation('softmax'),
+])
 
-# PLOTTING FUNCTIONS
-def plot_num_messages(chat_number):
-    plotted_data = final_data_messages[chat_number]
-    X = np.arange(len(plotted_data))
-    pl.bar(X, list(plotted_data.values()), align='center', width=0.5, color = 'r', bottom = 0.3)
-    pl.xticks(X, plotted_data.keys(), rotation = 90)
-    pl.title('Number of Messages Sent')
-    pl.tight_layout()
-    pl.show()
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
-def plot_histogram_time(chat_number):
-    person_to_times = final_data_times[chat_number]
-    pl.xlabel('Time')
-    pl.ylabel('Number of Messages')
-    pl.title('# of Messages Over Time')
-    colors = ['b', 'r', 'c', 'm', 'y', 'k', 'w', 'g']
-    for i , person in enumerate(person_to_times):
-        plotted_data = person_to_times[person]
-        pl.hist(plotted_data, 100, alpha=0.3, label=person, facecolor=colors[i % len(colors)])
-    pl.legend()
-    pl.xticks(rotation=90)
-    pl.tight_layout()
-    pl.show()
-
-def plot_histogram_words(chat_number):
-    temp = {}
-    for person in final_data_words[chat_number]:
-        temp[person] = np.average(final_data_words[chat_number][person])
-    plotted_data = temp
-    X = np.arange(len(plotted_data))
-    pl.bar(X, list(plotted_data.values()), align='center', width=0.5, color = 'r', bottom = 0.3)
-    pl.xticks(X, plotted_data.keys(), rotation = 90)
-    pl.title('Average Word Count')
-    pl.tight_layout()
-    pl.show()
-
-def plot(chat_number):
-    plot_num_messages(chat_number)
-    plot_histogram_time(chat_number)
-    plot_histogram_words(chat_number)
+model.fit(df, to_categorical(friend_vector), epochs=3)
 
 # CHECK AGAINST MODEL
 def check_against_model():
-    input = input("Send me a sample message:\n")
-    print([clean_word(word) for word in input.split()])
+    user_words = input("Send me a sample message:\n")
+    cleaned_input = [clean_word(word) for word in user_words.split()]
+    input_vector = np.zeros((len(significant_words)),)
+    for word in cleaned_input:
+        for count, word_check in enumerate(significant_words):
+            if word == word_check:
+                input_vector[count] = 1
+    predictionDF = pd.DataFrame(input_vector)
+    result = model.predict(predictionDF.T)
+    if result[1] > result[0]:
+        print("I think we're likely to have lots of messages!")
+    else:
+        print("We probably don't have many messages :(")
+
+check_against_model()
